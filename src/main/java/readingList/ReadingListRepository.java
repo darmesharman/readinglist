e9576822073262c83498e7d10ac3195390f95eab
Arman Darmesh <Darmesh.Ar@altynbank.kz>
2023-08-21 21:23:27 +0600

    Create ReadingList Controller

--- src/main/java/readingList/ReadingListRepository.java
+++ src/main/java/readingList/ReadingListRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-@SuppressWarnings("unused")
 public interface ReadingListRepository extends JpaRepository<Book, Long> {
 
     List<Book> findByReader(String reader);
