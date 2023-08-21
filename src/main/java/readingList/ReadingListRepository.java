13d5251fd54cdd1d5208599ddeda458a566082f8
Arman Darmesh <Darmesh.Ar@altynbank.kz>
2023-08-21 17:25:48 +0600

    Create Repository for Book

---
+++ src/main/java/readingList/ReadingListRepository.java
@@ -0,0 +1,12 @@
+package readingList;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+@SuppressWarnings("unused")
+public interface ReadingListRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByReader(String reader);
+
+}
