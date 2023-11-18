02d6bc454e29fa6f21569f9680b8883927d5d54e
Arman Darmesh <Darmesh.Ar@altynbank.kz>
2023-11-18 17:40:27 +0600

    ref: Get,Post Mapping

--- src/main/java/readingList/controller/ReadingListController.java
+++ src/main/java/readingList/controller/ReadingListController.java
@@ -3,9 +3,10 @@ package readingList.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import readingList.config.properties.AmazonProperties;
 import readingList.entity.Book;
 import readingList.repository.ReadingListRepository;
@@ -21,7 +22,7 @@ public class ReadingListController {
 
     private final ReadingListRepository readingListRepository;
 
-    @RequestMapping(value = "/{reader}", method = RequestMethod.GET)
+    @GetMapping(value = "/{reader}")
     public String readersBooks(@PathVariable("reader") String reader, Model model) {
         List<Book> readingList = readingListRepository.findByReader(reader);
 
@@ -31,7 +32,7 @@ public class ReadingListController {
         return "readingList";
     }
 
-    @RequestMapping(value = "/{reader}", method = RequestMethod.POST)
+    @PostMapping(value = "/{reader}")
     public String addToReadingList(@PathVariable("reader") String reader, Book book) {
         book.setReader(reader);
         readingListRepository.save(book);
