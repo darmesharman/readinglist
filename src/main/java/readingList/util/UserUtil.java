1826f5210ebb31ea2631ed0a365ef88f5bb646f6
Arman Darmesh <Darmesh.Ar@altynbank.kz>
2023-11-18 17:56:05 +0600

    getCurrentUser method

--- src/main/java/readingList/util/UserUtil.java
+++ src/main/java/readingList/util/UserUtil.java
@@ -1,8 +1,25 @@
 package readingList.util;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import readingList.entity.Reader;
+
+import java.util.Objects;
 
 @Service
 public class UserUtil {
+
+    public Reader getCurrentUser() throws AuthenticationException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (Objects.isNull(authentication) || !(authentication.getPrincipal() instanceof Reader)) {
+            throw new UsernameNotFoundException("No authenticated user");
+        }
+
+        return (Reader) authentication.getPrincipal();
+    }
 
 }
